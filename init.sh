a6c7cec759955b6de1e12435a6848d9a6471e9de
crynobone <crynobone@gmail.com>
2013-10-15 20:15:52 +0800

    Update script to pull from origin master
    Signed-off-by: crynobone <crynobone@gmail.com>

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-git submodule foreach git pull;
+git submodule foreach git pull origin master;
 cp docs/2.0/contents.md _includes/sidebar-docs2.0.md;
 cp docs/1.0/contents.md _includes/sidebar-docs1.0.md;
